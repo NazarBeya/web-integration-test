e6bd0fd523810aba70d26f3d5f18691a0f28e557
nazarBeya <nazarbeya@gmail.com>
2023-10-05 12:21:53 +0300

    add helper for header

---
+++ components/Header/helper.tsx
@@ -0,0 +1,12 @@
+import { Twitter, Twitch, TikTok, YouTube, Instagram, Facebook, Discord} from '@/desingSystem/index'
+
+
+export const iconsArr = [
+    { img: <Twitter/>},                
+    { img: <Twitch/>},
+    { img: <TikTok/>},
+    { img: <YouTube/>},
+    { img: <Instagram/>},
+    { img: <Facebook/>},
+    { img: <Discord/>},
+]
